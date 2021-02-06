4a0077d68df662f5bccdf432a2b3a84a3b792d4b
Chris Follen <christopher.follen@gmail.com>
2021-02-06 09:18:44 -0700

    commit tree d04632bece8cb5319204086cdd206b5ca85928a6

---
+++ app/models/bloodoath.rb
@@ -0,0 +1,21 @@
+class BloodOath
+
+    attr_reader :cult, :follower
+
+    @@all = []
+
+    def initialize cult, follower, initiation_date
+        @cult = cult
+        @follower = follower
+        @initiation_date = initiation_date
+
+        @@all << self
+
+    end
+
+    def self.all
+        @@all 
+    end
+
+    
+
